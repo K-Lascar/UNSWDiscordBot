f9528766356fcf0ba3e4704b5bf6c698b40acbbf
ChartingVolumes <36436810+ChartingVolumes@users.noreply.github.com>
2020-12-28 10:55:40 +1100

    Improved  location.py with the use of the function tile() in the string class

--- locations/location.py
+++ locations/location.py
@@ -1,11 +1,13 @@
 from geotext import GeoText
 from sys import argv
 
-def capitaliseArgs():
-    return " ".join(list(map(lambda x: x.capitalize(), argv[1:])))
+# We capitalise arguments because the dataset GeoText retrieves is in this form.
+def capitalise_args():
+    return "".join(argv[1:]).title()
 
 if __name__ == "__main__":
-    city = GeoText(capitaliseArgs()).cities
+    city = GeoText(capitalise_args()).cities
+
     if city != []:
         print(city[-1], end="")
     else:
